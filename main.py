b41570459b63a304394bfd31a45df89334c9d89b
Cody Pollard <codypollardeve@gmail.com>
2017-07-11 10:33:51 -0700

    Misc

---
+++ main.py
@@ -0,0 +1,14 @@
+import discord, token
+from discord.ext.commands import Bot
+
+my_bot = Bot(command_prefix="!")
+
+@my_bot.event
+async def on_ready():
+    print("Client logged in")
+
+@my_bot.command()
+async def hello(*args):
+    return await my_bot.say("Hello, world!")
+
+my_bot.run(token.token)
