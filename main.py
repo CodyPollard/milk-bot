b611b47b6f2a38ee6b3fd77c6255f04965e24da2
Cody Pollard <codypollardeve@gmail.com>
2017-07-11 13:16:09 -0700

    Added quotes

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-import discord, secrets
+import discord, secrets, quotes, random
 from discord.ext.commands import Bot
 
 my_bot = Bot(command_prefix="!")
+q = quotes.quote_list
+
 
 @my_bot.event
 async def on_ready():
     print("Client logged in")
 
+
 @my_bot.command()
-async def hello(*args):
-    return await my_bot.say("Hello, world!")
+async def anime(*args):
+    return await my_bot.say("kys")
+
+@my_bot.command()
+async def testing(*args):
+    c = my_bot.get_all_channels()
+    for i in c:
+        return await my_bot.say(i.name)
+
+@my_bot.command()
+async def quotes(*args):
+    return await my_bot.say(random.choice(q))
+
+
 
 my_bot.run(secrets.token_id)
+# print(private_channels)
---
+++ quotes.py
@@ -0,0 +1,4 @@
+quote_list = ['"Breakfast is an American conspiracy" - Christian Newell',
+              '"Fuck now I need a meme to be known for" - Leo']
+
+testing = "Testing"
