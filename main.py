7c46adaf46f343edc0a4e90d2d267cde2878d5f1
Cody Pollard <codypollardeve@gmail.com>
2017-07-12 11:00:38 -0700

    Fixed add command and added input sanitation for it

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ q = quotes.quote_list
 @my_bot.event
 async def on_ready():
     print("Client logged in")
+    quotes.update_quotes()
 
 @my_bot.command(pass_context=True)
 async def add(ctx, *args):
     msg = ctx.message.content
     auth = ctx.message.author
-    q.append(msg)
-
     try:
-        q.append(msg)
+        quotes.add_quote(msg)
         return await my_bot.say('Quote successfuly added.')
-    except:
-        return await my_bot.say('There was an error.')
+    except quotes.SanitationError as exception:
+        return await my_bot.say(exception)
 
 @my_bot.command()
 async def channels(*args):
@@ -30,7 +29,7 @@ async def channels(*args):
     return await my_bot.say('List of currently available channels: \n{}'.format(channels))
 
 @my_bot.command()
-async def quotes(*args):
+async def quote(*args):
     return await my_bot.say(random.choice(q))
 
 @my_bot.command()
@@ -41,15 +40,10 @@ async def close(*args):
 async def imgay(*args):
     return await my_bot.say('\nI M G A Y\nM\nG\nA\nY')
 
+@my_bot.command()
 async def info(*args):
-    return await my_bot.say('\nI M G A Y\nM\nG\nA\nY')
-
-
-
-def why():
-    my_bot.say("This is a meme")
-
-
+    return await my_bot.say('I am currently hosted at: https://github.com/CodyPollard/milk-bot\n'
+                            'To suggest features and track development type !ra for access to the bot-help channel.')
 
 
 my_bot.run(secrets.token_id)
--- quotes.py
+++ quotes.py
@@ -1,4 +1,37 @@
-quote_list = ['"Breakfast is an American conspiracy" - Christian Newell',
-              '"Fuck now I need a meme to be known for" - Leo']
+# quotes.py handels the reading and writing of quotes.txt
+# Used by the bot's !quote command
+quote_list = []
 
-testing = "Testing"
+class SanitationError(Exception):
+    pass
+
+# Open quotes.txt and read the contents into quote_list
+def update_quotes():
+    with open('quotes.txt', 'r') as f:
+        for line in f:
+            line.rstrip('\n')
+            quote_list.append(line)
+
+def add_quote(msg):
+
+    if '"' in msg:
+        print('This contains quotes')
+        new = msg.split(" ")
+        if '-' in new[len(new)-1]:
+            print("Thanks for adding an author")
+            with open('quotes.txt', 'a') as f:
+                f.write('\n')
+                for i in range(1, len(new)):
+                    f.write(new[i] + " ")
+            update_quotes()
+            return True
+        else:
+            raise SanitationError('Please add an author at the end of your quote.')
+    else:
+        raise SanitationError('Please use quotation marks when adding a quote.')
+
+
+
+if __name__ == "__main__":
+    update_quotes()
+    add_quote('!add quote "test')
